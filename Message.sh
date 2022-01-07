232bf74877b7dd46c200d0cf7b17b6fbec8d9f75
Saeed Nemati <sn.1361@gmail.com>
2022-01-07 10:31:17 +0330

    committed by script

---
+++ Message.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+red=`tput setaf 1`
+green=`tput setaf 2`
+cyan=`tput setaf 6`
+magenta=`tput setaf 5`
+yellow=`tput setaf 3`
+reset=`tput sgr0`
+
+function Success()
+{
+    echo "${green}$1{$reset}"
+}
+
+function Info()
+{
+    echo "${cyan}$1${reset}"
+}
+
+function Warning()
+{
+    echo "${yellow}$1${reset}"
+}
+
+function Error()
+{
+    echo "${red}$1${reset}"
+}
+
+function Divide()
+{
+    sudo echo ""
+    sudo echo "${magenta}----------${reset}"
+    sudo echo ""
+}
